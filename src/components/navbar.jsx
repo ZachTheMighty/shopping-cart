322df9a5a083e28ff090c3123dbe5c9031e7e222
ZachTheMighty <promoted66@gmail.com>
2026-08-07 17:49:10 +0300

    add: content to homepage

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,8 +2,8 @@ import { Menu, Search, ShoppingCart, UserRound } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <div className="px-8 py-4 flex flex-col items-center gap-8 sm:flex-row sm:justify-between ">
-      <div className="text-5xl font-semibold">Logo</div>
+    <div className="flex flex-col items-center gap-8 sm:flex-row sm:justify-between ">
+      <div className="text-5xl font-semibold">Epic Logo</div>
       <div className="bg-gray-100 h-full p-2 rounded-full">
         <Menu color="#6e6e70" className="hover:cursor-pointer" />
       </div>
