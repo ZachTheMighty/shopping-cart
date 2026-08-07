317326b6df353f1276f69a1d149b2341b0f8c7b3
ZachTheMighty <promoted66@gmail.com>
2026-08-07 21:21:03 +0300

    change: centered image only in product component

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -8,8 +8,12 @@ export default function Product({
   image,
 }) {
   return (
-    <div className="text-lg bg-gray-200 p-4 flex flex-col items-center ">
-      <img src={image} alt={title} className="w-50 h-50 object-contain" />
+    <div className="text-lg bg-gray-200 p-16 flex flex-col rounded-xl">
+      <img
+        src={image}
+        alt={title}
+        className="w-50 h-50 object-contain self-center"
+      />
       <div className="flex justify-between mt-8">
         <div className="text-gray-400 ">{category}</div>
         <div>
@@ -18,7 +22,7 @@ export default function Product({
         </div>
       </div>
       <div className="font-semibold mt-2">{title}</div>
-      <div>{description}</div>
+      <div className="mt-4">{description}</div>
       <div className="mt-2">
         <div className="text-gray-600">{count} pcs</div>
         <div>${price}</div>
