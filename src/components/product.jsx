39fc00b9884b504103ec4929b9016f5a628040a7
ZachTheMighty <promoted66@gmail.com>
2026-08-07 21:41:56 +0300

    refactor: managed showing hiding description using state instead

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ChevronRight, ChevronDown } from "lucide-react";
+
 export default function Product({
   title,
   description,
@@ -7,6 +10,8 @@ export default function Product({
   rate,
   image,
 }) {
+  const [showDesc, setShowDesc] = useState(false);
+
   return (
     <div className="text-lg bg-gray-200 p-16 flex flex-col rounded-xl">
       <img
@@ -23,7 +28,20 @@ export default function Product({
       </div>
       <div className="font-semibold mt-2">{title}</div>
       <div className="mt-4">
-        <details>{description}</details>
+        {showDesc ? (
+          <div onClick={() => setShowDesc(!showDesc)}>
+            <ChevronDown />
+            {description}
+          </div>
+        ) : (
+          <div
+            onClick={() => setShowDesc(!showDesc)}
+            className="flex items-center"
+          >
+            <ChevronRight />
+            <div>Description</div>
+          </div>
+        )}
       </div>
       <div className="mt-2">
         <div className="text-gray-600">{count} pcs</div>
