538794cb26ecc49d8012a785a1551a8d7b00fd41
ZachTheMighty <promoted66@gmail.com>
2026-08-08 19:03:14 +0300

    change: name of navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { Menu, Search, ShoppingCart, UserRound } from "lucide-react";
 export default function Navbar() {
   return (
     <div className="flex flex-col items-center gap-8 sm:flex-row sm:justify-between ">
-      <div className="text-5xl font-semibold">Epic Logo</div>
+      <div className="text-5xl font-semibold">Brandname</div>
       <div className="bg-gray-100 h-full p-2 rounded-full">
         <Menu color="#6e6e70" className="hover:cursor-pointer" />
       </div>
