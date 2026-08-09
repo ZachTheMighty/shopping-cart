7c4b060a5549cc40a38450ef2bd7595232cb9072
ZachTheMighty <promoted66@gmail.com>
2026-08-09 19:11:56 +0300

    add: static sort order button

--- src/components/sort.jsx
+++ src/components/sort.jsx
@@ -1,20 +1,39 @@
-import { ChevronDown } from "lucide-react";
+import {
+  ArrowDownWideNarrow,
+  ChevronDown,
+  ArrowUpWideNarrow,
+} from "lucide-react";
 import { useState } from "react";
 import SortingMethod from "./sorting_method.jsx";
 
 export default function Sort({ methods, value, setValue }) {
   const [showDropdown, setShowDropdown] = useState(false);
   const [checkedIndex, setCheckedIndex] = useState(-1);
+  const [descendingly, setDescendingly] = useState(true);
 
   return (
     <div className="relative">
-      <div
-        onClick={() => setShowDropdown((prevShow) => !prevShow)}
-        className="outline outline-gray-200 max-w-35 px-4 py-2 flex justify-between items-center rounded-md hover:shadow-[2px_5px_20px_-5px_rgba(0,0,0,0.1)] transition-shadow duration-200 cursor-pointer"
-      >
-        <div>{value}</div>
-        <ChevronDown size={20} />
+      <div className="flex items-center gap-2">
+        <div
+          onClick={() => setShowDropdown((prevShow) => !prevShow)}
+          className="outline outline-gray-200 max-w-35 px-4 py-2 flex justify-between items-center rounded-md hover:shadow-[2px_5px_20px_-5px_rgba(0,0,0,0.1)] transition-shadow duration-200 cursor-pointer"
+        >
+          <div>{value}</div>
+          <ChevronDown size={20} />
+        </div>
+        {descendingly ? (
+          <ArrowDownWideNarrow
+            className="text-gray-500 hover:text-black"
+            onClick={() => setDescendingly((prevDesc) => !prevDesc)}
+          />
+        ) : (
+          <ArrowUpWideNarrow
+            className="text-gray-500 hover:text-black"
+            onClick={() => setDescendingly((prevDesc) => !prevDesc)}
+          />
+        )}
       </div>
+
       {showDropdown && (
         <div className="outline outline-gray-200 px-4 py-2 w-50 rounded-md mt-3 text-sm absolute bg-white">
           <ul className="flex flex-col gap-1">
