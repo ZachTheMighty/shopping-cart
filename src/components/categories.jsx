6783362f7ab6b021ac1b74122e5f9920e65e5de1
ZachTheMighty <promoted66@gmail.com>
2026-08-10 16:45:22 +0300

    add: hover style to category links

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -6,10 +6,24 @@ export default function Categories() {
     <div className="px-8 py-4">
       <Navbar />
       <div className="flex flex-col items-center gap-2 sm:gap-0 sm:flex-row sm:justify-between md:justify-evenly mt-8 md:text-xl">
-        <Link to="/shop/categories/men's_clothing">Men's Clothing</Link>
-        <Link to="/shop/categories/women's_clothing">Women's clothing</Link>
-        <Link to="/shop/categories/jewelery">Jewelery</Link>
-        <Link to="/shop/categories/electronics">Eelectronics</Link>
+        <Link
+          to="/shop/categories/men's_clothing"
+          className="hover:text-teal-500"
+        >
+          Men's Clothing
+        </Link>
+        <Link
+          to="/shop/categories/women's_clothing"
+          className="hover:text-teal-500"
+        >
+          Women's clothing
+        </Link>
+        <Link to="/shop/categories/jewelery" className="hover:text-teal-500">
+          Jewelery
+        </Link>
+        <Link to="/shop/categories/electronics" className="hover:text-teal-500">
+          Eelectronics
+        </Link>
       </div>
     </div>
   );
