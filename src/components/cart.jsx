9d2ca9309c2830bea221ef72dfb1f7e5d7b36a69
ZachTheMighty <promoted66@gmail.com>
2026-08-13 15:41:09 +0300

    add: responsivness to empty cart page

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router";
 export default function Cart() {
   const { itemsInCart } = useOutletContext();
   return (
-    <div className="ml-4">
-      <div className="text-3xl font-semibold mb-8">Your Cart</div>
+    <div className="flex flex-col items-center sm:block sm:ml-16">
+      <div className="text-4xl font-semibold mb-8">Your Cart</div>
 
       {itemsInCart.length === 0 ? (
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col gap-2 items-center sm:items-start">
           <div className="text-xl">You don't have any items in cart.</div>
           <Link
             to="/shop"
-            className="bg-teal-700 text-white font-semibold text-xl px-4 py-2 rounded-md inline-block mx-auto hover:bg-teal-800 cursor-pointer"
+            className="bg-teal-700 text-white font-semibold text-xl px-4 py-2 rounded-md inline-block hover:bg-teal-800 cursor-pointer"
           >
             Add useless stuff
           </Link>
