aa000a8c56da0a00750f95871c4cfb97b71d596a
ZachTheMighty <promoted66@gmail.com>
2026-08-16 00:13:34 +0300

    fix: nav icons container not centered
    this was because the other flex items in the navbar; brandname and the shop now divs, had different widths to each other, so for example if brandname div was wider the shop now div, the icons div would be shifted to the right and vice versa. fixed by applying flex-1 to all flex items so they have a consistent equal width

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,12 +6,12 @@ export default function Navbar({ numOfItems = 0 }) {
     <div className="flex flex-col items-center gap-8 sm:flex-row sm:justify-between px-16 py-8">
       <Link
         to="/"
-        className="inline-block text-3xl md:text-5xl font-semibold hover:text-gray-700"
+        className="inline-block text-3xl sm:text-4xl lg:text-5xl font-semibold hover:text-gray-700 sm:flex-1"
       >
         Brandname
       </Link>
 
-      <div className="flex items-center gap-8">
+      <div className="flex items-center justify-center gap-8 sm:flex-1">
         <Search className="hover:cursor-pointer" />
         <Link to="cart" className="relative">
           <ShoppingCart className="hover:cursor-pointer" />
@@ -25,16 +25,16 @@ export default function Navbar({ numOfItems = 0 }) {
         </Link>
         <UserRound className="hover:cursor-pointer" />
       </div>
-      <div className="flex flex-col items-center gap-4 md:flex-row">
+      <div className="flex flex-col items-center gap-4 sm:flex-row sm:justify-end sm:flex-1">
         <Link
           to="shop"
-          className="bg-teal-700 text-white font-semibold px-8 py-2 rounded-md hover:bg-teal-800"
+          className="bg-teal-700 text-white font-semibold px-4 py-2 rounded-md hover:bg-teal-800 flex-1 text-center lg:flex-none"
         >
           Shop now
         </Link>
         <Link
           to="shop/categories"
-          className="text-teal-700 font-semibold hover:text-teal-500 text-center"
+          className="text-teal-700 font-semibold hover:text-teal-500 text-center flex-1 lg:flex-none"
         >
           Browse our catalogue
         </Link>
